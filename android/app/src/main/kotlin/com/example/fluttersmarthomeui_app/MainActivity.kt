6b0d82a3ef413f57d6dd2e3e88ced73076935437
= <=>
2021-06-17 18:22:38 +0530

    alreay commit

---
+++ android/app/src/main/kotlin/com/example/fluttersmarthomeui_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fluttersmarthomeui_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = Color(0xFF26282B);
+Color activeColor1 = Color(0xFFED560D);
+Color activeColor2 = Color(0xFFcd2a12);
+
+List<Color> gradientColor = [activeColor2,activeColor2];
